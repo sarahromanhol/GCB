9730bf62e22fc12e473e1a40d89e4d3fc51deb3d
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2021-12-12 18:44:44 -0300

    Header finalizado

--- healthyfood/src/components/Header/Header.js
+++ healthyfood/src/components/Header/Header.js
@@ -3,17 +3,17 @@ import * as S from './styled'
 import { useCoordinator } from "../../hooks/useCoordinator"
 
 
-export const Header = () => {
+export const Header = ({ativaCor}) => {
     const goTo = useCoordinator()
 
     return (
-        <S.HeaderContainer>
-            <h2 onClick={goTo.LandingPage}>Healthy Food</h2>
+        <S.HeaderContainer ativaCor={ativaCor}>
+            <S.Logo ativaCor={ativaCor} onClick={goTo.LandingPage}>Healthy Food</S.Logo>
             <S.ButtonsContainer>
                 <S.GeneralHeaderButton>HEALTHY RECIPES</S.GeneralHeaderButton>
                 <S.GeneralHeaderButton>BLOG</S.GeneralHeaderButton>
                 <S.GeneralHeaderButton>JOIN</S.GeneralHeaderButton>
-                <button onClick={goTo.Register} >REGISTER</button>
+                <S.RegisterButton onClick={goTo.Register} >REGISTER</S.RegisterButton>
             </S.ButtonsContainer>
         </S.HeaderContainer>
     )
